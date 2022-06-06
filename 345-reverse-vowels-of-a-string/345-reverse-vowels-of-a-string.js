16b7a323b63ca104b4a59be6b306ec9d377d27a6
Shahd Maqboul <103340891+shahdMaqboul@users.noreply.github.com>
2022-06-06 16:46:57 +0300

    Time: 172 ms (17.52%), Space: 49.5 MB (40.57%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.js
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.js
@@ -2,10 +2,8 @@
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function(s1) {
-    if(s1==null) return s1;
-    
-    const s=s1.split("");
+var reverseVowels = function(s) {
+    s=s.split("");
     
     const vowels =['a', 'e', 'i', 'o', 'u'];
     const arr = []
@@ -15,13 +13,14 @@ var reverseVowels = function(s1) {
         if(vowels.includes(s[i]) || vowels.includes(s[i].toLowerCase()))
             arr.push(i)
     }
-    console.log(arr)
+    
     for(let i=0;i<arr.length/2;i++){
         aux = s[arr[i]];
         s[arr[i]]=s[arr[arr.length-1-i]]
         s[arr[arr.length-1-i]]=aux;
         
     }
+    
     return s.join("");
     
 };
