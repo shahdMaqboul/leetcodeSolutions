7297b037b5aec3924fd06b5aa3044e4aa6f54d48
Shahd Maqboul <103340891+shahdMaqboul@users.noreply.github.com>
2022-06-06 17:14:41 +0300

    Time: 114 ms (18.14%), Space: 42.3 MB (92.99%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -7,19 +7,19 @@ var intersect = function(nums1, nums2) {
     var minLen = Math.min(nums1.length,nums2.length);
     const arr=[];
     
-    for(let i=0 ; i < nums2.length ;i++){
-        // if(minLen == nums1.length){
-            // if(nums2.indexOf(nums1[i])!= -1){
-            //     nums2[nums2.indexOf(nums1[i])]=""
-            //     arr.push(nums1[i])
-            // }
-        // }
-        // else{
+    for(let i=0 ; i < minLen ;i++){
+        if(minLen == nums1.length){
+            if(nums2.indexOf(nums1[i])!= -1){
+                nums2[nums2.indexOf(nums1[i])]=""
+                arr.push(nums1[i])
+            }
+        }
+        else{
             if(nums1.indexOf(nums2[i])!= -1){
                 nums1[nums1.indexOf(nums2[i])]=""
                 arr.push(nums2[i])
             }
-        // }
+        }
     }
     return arr;
     
