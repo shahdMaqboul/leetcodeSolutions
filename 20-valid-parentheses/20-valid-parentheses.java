df93d288f4a282785f41400c2fbd5b8b0ecb8b36
Shahd Maqboul <103340891+shahdMaqboul@users.noreply.github.com>
2022-06-22 16:04:48 +0300

    Time: 13 ms (5.74%), Space: 42.4 MB (23.95%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,22 +1,16 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length() == 0) return true;
-        if(s.length() == 1) return false;
-        String p;
         Stack <String> st = new Stack<String>();
         String [] arr = s.split("");
         for(int i =0;i<arr.length;i++){
-            if(arr[i].equals(")") || arr[i].equals("}") || arr[i].equals("]")){
-                if(st.size() == 0) return false;
-                p=st.pop();
-                if((arr[i].equals(")") && !p.equals("("))
-                   || (arr[i].equals("}") && !p.equals("{"))
-                    || (arr[i].equals("]") && !p.equals("["))){
+                if(arr[i].equals("(")) st.push(")");
+                else if (arr[i].equals("[")) st.push("]");
+                else if (arr[i].equals("{")) st.push("}");
+                else if (st.size() != 0){
+                    if(arr[i].equals(st.pop()) == false) return false;
+                }else{
                     return false;
-                }
-            }else{
-                st.push(arr[i]);
-            }
+                }    
         }
         if(st.size() == 0)
         return true;
