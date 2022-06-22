ca15cb7282a5b999283d8c71f8e57e162900b540
Shahd Maqboul <103340891+shahdMaqboul@users.noreply.github.com>
2022-06-22 20:00:26 +0300

    Time: 0 ms (100.00%), Space: 43.8 MB (22.29%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,17 +1,21 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        int position = 0;
         if(nums.length ==0)return 0;
+        else if(target<nums[0]) return 0;
+        else if(target>nums[nums.length-1]) return nums.length;
         
             for(int i =0;i< nums.length;i++){
                 if(nums[i]==target) 
                     return i;
+                if(position == 0){
+                    if(nums[i]>=target) 
+                        position = i;
+                    // else if(i==nums.length-1)
+                    //     position = i+1;
+                }
             }
-           for(int i =0;i< nums.length;i++){
-                if(nums[i]>=target) 
-                    return i;
-               else if(i==nums.length-1)
-                   return i+1;
-            }
-        return 0 ;
+           
+        return position ;
     }
 }
