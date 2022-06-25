b02dc42d0259b3c0157e28fe9a2f509e49b0b37b
Shahd Maqboul <103340891+shahdMaqboul@users.noreply.github.com>
2022-06-25 16:27:28 +0300

    Time: 1 ms (11.19%), Space: 42.9 MB (49.03%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -14,7 +14,6 @@ class Solution {
         
         Stack<ListNode> s = new Stack<ListNode>();
         ListNode n = head;
-        ListNode j;
         
         while(n.next!=null){
             s.push(n);
@@ -22,24 +21,14 @@ class Solution {
         }
         
         head=n;
-        printList(head);
         
         while(!s.empty()){
             n.next = s.pop();
-            n.next.next = null;
+            n.next.next = null; //to avoid having circular linked list, beacuse we pop the node having value and next
             n=n.next;
         }
         
-        printList(head);
-        
         return head;
         
-    }
-    void printList(ListNode node)
-    {
-        while (node != null) {
-            System.out.print(node.val + " ");
-            node = node.next;
-        }
     }
 }
