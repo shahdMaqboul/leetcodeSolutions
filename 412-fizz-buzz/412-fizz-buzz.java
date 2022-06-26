75cbb8c95fb2c8706ea8ad69f9b516b0cf949670
Shahd Maqboul <103340891+shahdMaqboul@users.noreply.github.com>
2022-06-26 12:34:59 +0300

    Time: 1 ms (100.00%), Space: 49.3 MB (5.08%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> array = new ArrayList<String>();
+        for(int i =1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                array.add("FizzBuzz");
+            }else if(i%3==0){
+                array.add("Fizz");
+            }else if(i%5==0){
+                array.add("Buzz");
+            }else{
+                array.add(Integer.toString(i));
+            }
+        }
+        return array;
+        
+    }
+}
