9abef132eb53bfef0d206c816c0aae538dcd1438
Shahd Maqboul <103340891+shahdMaqboul@users.noreply.github.com>
2022-06-26 12:22:50 +0300

    Time: 22 ms (25.43%), Space: 100.7 MB (36.42%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head==null || head.next==null)return true;
+        if(head==null || head.next==null) return true;
         
         Stack<Integer> s = new Stack<Integer>();
         ListNode n1 = head;
@@ -20,24 +20,15 @@ class Solution {
             s.push(n1.val);
             n1=n1.next;
             n2=n2.next.next;
-            if(n2 != null && n2.next==null){
+            if(n2 != null && n2.next==null){ //odd number of nodes
                 n1=n1.next;
             }
         }
         
-        for(int i=0;i<s.size();i++){
-            System.out.println(s.get(i));
-        }
-        // System.out.println(n1.val);
-        // System.out.println(n2);
-        
-        System.out.println("size before"+s.size());
         for(int i=0;i<=s.size();i++){
-            System.out.println("i= "+i);
             if(n1.val != s.pop()) return false;
             n1=n1.next;
         }
-        System.out.println("size after"+s.size());
 
         return true;
     }
