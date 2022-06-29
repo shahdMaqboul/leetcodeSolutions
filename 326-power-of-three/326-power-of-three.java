5551afaed415f762e1db9178e88e3020a253a69f
Shahd Maqboul <103340891+shahdMaqboul@users.noreply.github.com>
2022-06-29 12:53:26 +0300

    Time: 28 ms (18.55%), Space: 47.2 MB (59.40%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -4,14 +4,12 @@ class Solution {
         
         if(n <=0 ) return false;
         
-        while(n!=1 && n!= -1){
-            System.out.println(n);
-            if(n%3 != 0){
-                System.out.println("n%3"+ n%3);
-                return false;
-                }
+        while(n!=1){
+            if(n%3 != 0) 
+                return false; 
             n=n/3;
         }
+        
         return true;
     
     }
