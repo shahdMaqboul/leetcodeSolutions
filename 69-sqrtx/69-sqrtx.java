9a85a28e91aa91cefe4045a1d1275c333e0304c3
Shahd Maqboul <103340891+shahdMaqboul@users.noreply.github.com>
2022-07-02 15:05:15 +0300

    Time: 79 ms (5.04%), Space: 41.7 MB (29.73%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -12,3 +12,29 @@ class Solution {
         
     }
 }
+
+//another solution for the same problem
+// https://www.goodtecher.com/leetcode-69-sqrtx/
+
+// class Solution {
+//     public int mySqrt(int x) {
+//         long start = 0;
+//         long end = x;
+        
+//         while (start + 1 < end) {
+//             long mid = start + (end - start) / 2;
+//             if (mid * mid == x) {
+//                 return (int)mid;
+//             } else if (mid * mid < x) {
+//                 start = mid;
+//             } else {
+//                 end = mid;
+//             }
+//         }
+        
+//         if (end * end == x) {
+//             return (int)end;
+//         }
+//         return (int)start;
+//     }
+// }
