0111257148d98657b114d41bf19c0c2961e7b1dd
Shahd Maqboul <103340891+shahdMaqboul@users.noreply.github.com>
2022-07-03 12:23:02 +0300

    Time: 1 ms (100.00%), Space: 48 MB (29.46%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -3,11 +3,10 @@ class Solution {
         int count=0;
         
         for(int i = 0; i< nums.length ;i++){
-            // If the current element is equal to the next element, we skip
             if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
                 continue;
             }
-            // We will update the array in place
+           
             nums[count] = nums[i];
             count++;
         }
