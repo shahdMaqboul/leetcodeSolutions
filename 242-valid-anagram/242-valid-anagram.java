559f20417a63cc9420c93990347503c910ebe6fa
Shahd Maqboul <103340891+shahdMaqboul@users.noreply.github.com>
2022-07-05 12:38:28 +0300

    Time: 7 ms (53.09%), Space: 44.9 MB (37.86%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char s1[]= s.toCharArray();
+        Arrays.sort(s1);
+        
+        char t1[]= t.toCharArray();
+        Arrays.sort(t1);
+        
+        if(s1.length != t1.length) return false;
+        
+        for(int i=0 ; i<s1.length ; i++){
+            if(s1[i]!=t1[i]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
